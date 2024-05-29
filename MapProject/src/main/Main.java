afcf4397990a1911cd6859b3f11e4f96af7a25a6
binhbb2204 <binhbb2204@gmail.com>
2024-05-29 17:04:52 +0700

    testing

---
+++ MapProject/src/main/Main.java
@@ -0,0 +1,11 @@
+
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hello");
+    }
+    
+}
