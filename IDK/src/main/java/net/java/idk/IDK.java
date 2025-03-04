3413c5d6089e12fdc14a5be7b5ca027d3470d982
binhbb2204 <binhbb2204@gmail.com>
2025-03-04 16:50:28 +0700

    gg

---
+++ IDK/src/main/java/net/java/idk/IDK.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package net.java.idk;
+
+/**
+ *
+ * @author binhb
+ */
+public class IDK {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
